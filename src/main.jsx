a55448a29f6e2e68c9dfc7bfcdb223970136978c
nasimrifat101 <nasimrifat101@gmail.com>
2023-09-24 21:26:13 +0600

    empty component and router path created

--- src/main.jsx
+++ src/main.jsx
@@ -6,15 +6,28 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import ErrorPage from './components/ErrorPage/ErrorPage';
+import Home from './components/Home/Home';
+import DonationDetails from './components/DonationDetails/DonationDetails';
+import Donation from './components/Donation/Donation';
+import Statistcs from './components/Statistcs/Statistcs';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Home></Home>,
     errorElement: <ErrorPage></ErrorPage>,
     children:  [
       {
-        
+        path:'/details',
+        element:<DonationDetails></DonationDetails>
+      },
+      {
+        path: '/donation',
+        element:<Donation></Donation>
+      },
+      {
+        path: '/statistics',
+        element: <Statistcs></Statistcs>
       }
     ]
   },
